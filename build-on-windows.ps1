cc85c7298204c1ca1b273f850464544ae5cadcdb
Bert Hertogen <bert@hertogen.net>
2022-05-16 17:00:13 +0200

    Init

---
+++ build-on-windows.ps1
@@ -0,0 +1,37 @@
+$workingDir = "./standalone-win-64";
+$assets = "./assets";
+
+if (Test-Path $workingDir) {
+    Write-Host "*** Cleaning $workingDir"
+    Remove-Item $workingDir -Recurse -Force | Out-Null
+}
+Write-Host "*** Creating $workingDir"
+New-Item -ItemType "directory" -Path $workingDir | Out-Null
+
+if (!(Test-Path $assets)) {
+    Write-Host "*** Creating $assets"
+    New-Item -ItemType "directory" -Path $assets | Out-Null
+}
+
+$workingDir = Resolve-Path -Path "./standalone-win-64";
+$assets = Resolve-Path -Path "./assets";
+
+Write-Host "*** Downloading gtk3 (https://github.com/tschoonj/GTK-for-Windows-Runtime-Environment-Installer)"
+Invoke-Expression "git clone https://github.com/tschoonj/GTK-for-Windows-Runtime-Environment-Installer.git $workingDir/gtk3/tmp-repo"
+Move-Item -Path "$workingDir/gtk3/tmp-repo/gtk-nsis-pack/*" -Destination "$workingDir/gtk3"
+Remove-Item $workingDir/gtk3/tmp-repo -Recurse -Force | Out-Null
+
+Write-Host "*** Downloading python (https://github.com/indygreg/python-build-standalone)"
+Invoke-WebRequest -Uri "https://github.com/indygreg/python-build-standalone/releases/download/20220502/cpython-3.10.4+20220502-x86_64-pc-windows-msvc-shared-install_only.tar.gz" -OutFile "$workingDir/python.tar.gz"
+Invoke-Expression "tar -xvzf $workingDir/python.tar.gz -C $workingDir"
+Remove-Item "$workingDir/python.tar.gz" -Recurse -Force | Out-Null
+
+Set-Location  "$workingDir/python"
+Write-Host "*** Installing weasyprint"
+Invoke-Expression ".\python.exe -m pip install weasyprint"
+$Env:PATH += ";$workingDir/gtk3/bin"
+Write-Host "*** Testing weasyprint"
+Invoke-Expression ".\python.exe -m weasyprint --info"
+Set-Location  "../../"
+
+Compress-Archive -Path "$workingDir/*"  -DestinationPath "$assets/standalone-win-64.zip" -CompressionLevel "Fastest" -Force
---
+++ readme.md
@@ -0,0 +1,34 @@
+# Usage
+
+TODO
+
+# Building assets
+
+## Windows (build-on-windows.ps1 does approximately this)
+
+* Download https://github.com/tschoonj/GTK-for-Windows-Runtime-Environment-Installer/releases and install to C:\weasyprint.wrapped\standalone-win-64\gtk
+* Download https://github.com/indygreg/python-build-standalone/releases (correct release, eg: cpython-3.10.4+20220502-x86_64-pc-windows-msvc-shared-install_only.tar.gz for windows) and extract to c:\weasyprint.wrapped\standalone-win-64\python
+* Add gtk3 to path (in current session) 
+``` powershell
+$Env:PATH += ";C:\weasyprint.wrapped\standalone-win-64\gtk3\bin"
+```
+* Install weasyprint in the standalone python
+```
+cd c:\weasyprint.wrapped\standalone-win-64\python
+.\python.exe -m pip install weasyprint
+.\python.exe -m weasyprint --info
+```
+
+## Help
+
+* Check PATH in python
+```
+.\python.exe -c "import os; print(*os.environ['PATH'].split(os.pathsep), sep='\n')"
+```
+
+# Externals
+
+* https://github.com/balbarak/WeasyPrint-netcore
+* https://github.com/Kozea/WeasyPrint
+* https://wiki.python.org/moin/EmbeddedPython
+* https://github.com/tschoonj/GTK-for-Windows-Runtime-Environment-Installer/releases
