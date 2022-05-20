39826a276d78be9c6806d6017a21f05de3b7756a
Bert Hertogen <bert@hertogen.net>
2022-05-20 16:55:51 +0200

    Update package size

--- build-on-windows.ps1
+++ build-on-windows.ps1
@@ -17,9 +17,12 @@ $workingDir = Resolve-Path -Path "./standalone-windows-64";
 $assets = Resolve-Path -Path "./assets";
 
 Write-Host "*** Downloading gtk3 (https://github.com/tschoonj/GTK-for-Windows-Runtime-Environment-Installer)"
-Invoke-Expression "git clone https://github.com/tschoonj/GTK-for-Windows-Runtime-Environment-Installer.git $workingDir/gtk3/tmp-repo"
-Move-Item -Path "$workingDir/gtk3/tmp-repo/gtk-nsis-pack/*" -Destination "$workingDir/gtk3"
-Remove-Item $workingDir/gtk3/tmp-repo -Recurse -Force | Out-Null
+Invoke-WebRequest -Uri "https://github.com/tschoonj/GTK-for-Windows-Runtime-Environment-Installer/releases/download/2022-01-04/gtk3-runtime-3.24.31-2022-01-04-ts-win64.exe" -OutFile "$workingDir/gtk3.exe"
+Invoke-Expression "$workingDir/gtk3.exe /setpath=no /dllpath_silent=yes /sideeffects=no /dllpath=root /translations=no /removeold=no /S /D=$workingDir\gtk3\install\"
+# Previous command is not blocking but should be done in 10 seconds
+Start-Sleep -Seconds 10
+Move-Item -Path "$workingDir/gtk3/install/*" -Destination "$workingDir/gtk3"
+Remove-Item $workingDir/gtk3/install -Recurse -Force | Out-Null
 
 Write-Host "*** Downloading python (https://github.com/indygreg/python-build-standalone)"
 Invoke-WebRequest -Uri "https://github.com/indygreg/python-build-standalone/releases/download/20220502/cpython-3.10.4+20220502-x86_64-pc-windows-msvc-shared-install_only.tar.gz" -OutFile "$workingDir/python.tar.gz"
